a4a36633bc4532c427d8a20a28dd37b5e2b2bf62
Chetan Bhoi <bhoichetan23@gmail.com>
2026-02-04 23:11:08 +0530

    Adding middlewares

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/UserModel");
+
+const protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Not authorized, token missing" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user; // full user object
+    next();
+  } catch (err) {
+    console.log("JWT error:", err.message);
+    return res.status(401).json({ message: "Token invalid or expired" });
+  }
+};
+
+module.exports = protect;
